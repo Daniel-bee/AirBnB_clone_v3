c33327db54936dfd414fdb9db604fdca5047e396
Obed101 <obedamoako92@gmail.com>
2022-02-04 11:10:36 +0000

    get and count

--- home.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, url_for
-
-appFlask = Flask(__name__)
-
-@appFlask.route('/home')
-def home():
-return 'We are in Home Page!'
-with appFlask.test_request_context():
-print(url_for('login'))
