e18187d84d0da042334a7414307b78b1a11500f5
Daniel-bee <daniel123_86@live.com>
2022-02-04 11:14:49 +0300

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,10 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
+    def get(self, cls, id):
+        pass
+    def count(self, cls=None):
+        pass
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
