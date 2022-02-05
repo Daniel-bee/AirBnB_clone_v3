7e77cd4e1139f92f2817fb357278599214aa5aca
Daniel-bee <daniel123_86@live.com>
2022-02-05 16:56:50 +0300

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
+import json
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exc):
+    """close session"""
     storage.close()
 
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv("HBNB_API_PORT")
-    if host == port:
-        app.run(host=host, port=port, threaded=True)
-    else:
-        app.run(host='0.0.0.0', port='5000', threaded=True)
+    host_ = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port_ = getenv("HBNB_API_PORT", default=5000)
+    app.run(host=host_, port=port_, threaded=True)
