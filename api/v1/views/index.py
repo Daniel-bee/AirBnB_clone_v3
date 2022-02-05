7b657c8722d65b44b504dc61b4ae8af16713e66a
Obed101 <obedamoako92@gmail.com>
2022-02-05 19:06:29 +0000

    count all

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,17 @@
 """Index view file """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def index():
     """Returns a JSON """
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/vi/stats')
+def obj_count():
+    """counts the number of objects in each class"""
+    for clss in storage.classes:
+        print(jsonify({clss: storage.all.count()}))
+        return
