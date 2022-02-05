f4eb331a7d1d3e05db9470cac1b2bb1ea3e868a0
Obed101 <obedamoako92@gmail.com>
2022-02-05 19:28:34 +0000

    404 error not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ main app file """
-from flask import Flask
-#from models import storage
+from flask import Flask, make_response
+from models import storage
 from api.v1.views import app_views
 from os import getenv
 
@@ -15,6 +15,10 @@ def teardown(exc):
     """close session"""
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     host_ = getenv('HBNB_API_HOST', default='0.0.0.0')
