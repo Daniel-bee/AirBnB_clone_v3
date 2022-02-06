91678b4fd19710802b17a9b5ac44fdb8f6d0b1f9
Daniel-bee <daniel123_86@live.com>
2022-02-06 12:07:47 +0300

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,14 +14,11 @@ def index():
 @app_views.route('/stats')
 def obj_count():
     """counts the number:
-    of objects in each class"""
-    classlist = []
-    for value in storage.all().values():
-        classlist.append(value.__class__)
-    dict_ = {"amenitys": storage.count(classlist[0]),
-             "cities": storage.count(classlist[1]),
-             "places": storage.count(classlist[2]),
-             "reviews": storage.count(classlist[3]),
-             "states": storage.count(classlist[4]),
-             "users": storage.count(classlist[5])}
-    return jsonify(dict_)
+    of objects in each class
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
