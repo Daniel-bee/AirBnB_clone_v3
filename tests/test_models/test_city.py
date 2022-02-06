f5a9e98f91ff1d0541ea7dce0bd75c09a6cf88b0
Daniel-bee <daniel123_86@live.com>
2022-02-06 23:01:55 +0300

    test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -112,3 +112,23 @@ class TestCity(unittest.TestCase):
         city = City()
         string = "[City] ({}) {}".format(city.id, city.__dict__)
         self.assertEqual(string, str(city))
+
+    def test_attr(self):
+        """Test for attribute"""
+        attr_list = ['state_id', 'name']
+        is_attr = False
+        for attri in attr_list:
+            is_attr = hasattr(City, attri)
+            if not is_attr:
+                break
+        self.assertEqual(is_attr, True)
+
+    def test_type_state_id(self):
+        """Test the type of state id attribute"""
+        type_id = type(City.state_id).__name__
+        self.assertEqual(type_id, "str")
+
+    def test_type_name(self):
+        """Test the type of name attribute"""
+        type_ = type(City.name).__name__
+        self.assertEqual(type_, "str")
