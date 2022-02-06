6995677ad6912c49479c268d56890ec59c4db643
Daniel-bee <daniel123_86@live.com>
2022-02-06 22:41:48 +0300

    new test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -104,3 +104,10 @@ class TestAmenity(unittest.TestCase):
         amenity = Amenity()
         string = "[Amenity] ({}) {}".format(amenity.id, amenity.__dict__)
         self.assertEqual(string, str(amenity))
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['file1.py', 'file2.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
