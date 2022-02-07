e13e77cff9492afd2d24bc709cc5ba2c2d8b337f
Daniel-bee <daniel123_86@live.com>
2022-02-07 16:14:52 +0300

    fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -53,8 +53,8 @@ def create_city(state_id):
         abort(404)
     if not request.json:
         abort(400, "Not a JSON")
-    elif 'name' not in request.json:
-        abort(400, 'Missing name')
+    elif "name" not in request.json:
+        abort(400, "Missing name")
     else:
         dict_ = {'state_id': state_id}
         for key, value in request.json.items():
