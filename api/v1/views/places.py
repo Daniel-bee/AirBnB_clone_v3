2b0b178abaaac26471d9884b83bbd36a44c91202
Obed101 <obedamoako92@gmail.com>
2022-02-08 00:38:32 +0000

    overwiting removed

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -23,7 +23,7 @@ def get_place(city_id):
 
 @app_views.route('/places/<place_id>', methods=["GET"],
                  strict_slashes=False)
-def get_place(place_id):
+def get_places(place_id):
     """list a place"""
     plc = storage.get(place.Place, place_id)
     if plc:
